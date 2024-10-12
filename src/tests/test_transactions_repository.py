aed2ac5b6f3c63a0dade999e4027e5e8b020b5c6
Wagner Maciel <wagner.varginha@hotmail.com>
2024-10-12 08:37:26 -0300

    test(TestTransactionsRepository): unit test for retrieving transaction

--- src/tests/test_transactions_repository.py
+++ src/tests/test_transactions_repository.py
@@ -1,5 +1,7 @@
 import unittest
+from uuid import uuid4
 
+from exceptions import NotFoundException
 from tests.helpers import FakeDataGenerator
 from transaction import Transaction
 from transactions_repository import TransactionsRepository
@@ -10,7 +12,7 @@ class TestTransactionsRepository(unittest.TestCase):
         self._repository = TransactionsRepository()
 
     def tearDown(self) -> None:
-        pass
+        self._repository = None
 
     def test_create_transaction(self) -> None:
         transaction = Transaction(**{
@@ -23,3 +25,17 @@ class TestTransactionsRepository(unittest.TestCase):
         created_transaction = self._repository.create_transaction(transaction)
         self.assertEqual(created_transaction.model_dump(exclude={"amount"}), transaction.model_dump(exclude={"amount"}))
         self.assertAlmostEqual(created_transaction.amount, transaction.amount)
+
+    def test_get_transaction(self) -> None:
+        transaction = Transaction(**{
+            "amount": FakeDataGenerator.positive_float(),
+            "date": FakeDataGenerator.date(),
+            "type": "income",
+            "category": FakeDataGenerator.string(5),
+            "description": None
+        })
+        created_transaction = self._repository.create_transaction(transaction)
+        self.assertIsNotNone(created_transaction)
+        retrieved_transaction = self._repository.get_transaction(created_transaction.id)
+        self.assertEqual(retrieved_transaction, created_transaction)
+        self.assertRaises(NotFoundException, self._repository.get_transaction, uuid4())
