b277e8db9636c50f190510e4211709a1bb3855ae
Wagner Maciel <wagner.varginha@hotmail.com>
2024-10-11 23:19:29 -0300

    test(TestTransaction): add unit testing for invalid Transaction creation

--- src/tests/test_transaction.py
+++ src/tests/test_transaction.py
@@ -38,6 +38,40 @@ class TestTransaction(unittest.TestCase):
         self._validate_transaction(transaction_data_2)
         self._validate_transaction(transaction_data_3)
 
+    def test_invalid_transaction(self) -> None:
+        transaction_data_1 = {
+            "amount": 0.0,
+            "date": FakeDataGenerator.date(),
+            "type": "income",
+            "category": FakeDataGenerator.string(5),
+            "description": None
+        }
+        transaction_data_2 = {
+            "amount": FakeDataGenerator.float(),
+            "date": FakeDataGenerator.date(),
+            "type": FakeDataGenerator.string(3),
+            "category": FakeDataGenerator.string(5),
+            "description": None
+        }
+        transaction_data_3 = {
+            "amount": FakeDataGenerator.float(),
+            "date": FakeDataGenerator.date(),
+            "type": "income",
+            "category": FakeDataGenerator.string(5),
+            "description": ""
+        }
+        transaction_data_4 = {
+            "amount": FakeDataGenerator.float(),
+            "date": FakeDataGenerator.date(),
+            "type": "expense",
+            "category": FakeDataGenerator.string(5),
+            "description": FakeDataGenerator.string(101)
+        }
+        self.assertRaises(ValueError, Transaction, **transaction_data_1)
+        self.assertRaises(ValueError, Transaction, **transaction_data_2)
+        self.assertRaises(ValueError, Transaction, **transaction_data_3)
+        self.assertRaises(ValueError, Transaction, **transaction_data_4)
+
     def _validate_transaction(self, transaction_data: dict) -> None:
         transaction = Transaction(**transaction_data)
         self.assertEqual(transaction.model_dump(exclude={"id"}), transaction_data)
