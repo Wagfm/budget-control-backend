01710be92bc78a8f458bc8284fd16c0e0bf072e0
Wagner Maciel <wagner.varginha@hotmail.com>
2024-10-12 08:38:39 -0300

    feat(TransactionsRepository): add transaction retrieval by id

--- src/transactions_repository.py
+++ src/transactions_repository.py
@@ -1,10 +1,12 @@
 import os
 from collections.abc import Sequence
 from typing import Any
+from uuid import UUID
 
 import psycopg_pool as pgp
 from psycopg import Cursor
 
+from exceptions import NotFoundException
 from transaction import Transaction
 
 
@@ -19,7 +21,8 @@ class DictRowFactory:
 class TransactionsRepository:
     def __init__(self) -> None:
         self._row_factory = DictRowFactory
-        self._pool = pgp.ConnectionPool(os.getenv("POSTGRES_URL"), min_size=3, max_size=10, open=True)
+        self._url = os.getenv("POSTGRES_URL")
+        self._pool = pgp.ConnectionPool(self._url, min_size=4, max_size=100, open=True)
 
     def create_transaction(self, transaction: Transaction) -> Transaction:
         query = """
@@ -35,3 +38,16 @@ class TransactionsRepository:
             result = cursor.execute(query, parameters)
             created_transaction_data = result.fetchone()
             return Transaction(**created_transaction_data)
+
+    def get_transaction(self, transaction_id: UUID) -> Transaction:
+        query = """
+            SELECT * FROM public.transactions WHERE id = %s;
+        """
+        parameters = [transaction_id]
+        with self._pool.connection() as connection:
+            cursor = connection.cursor(row_factory=self._row_factory)
+            result = cursor.execute(query, parameters)
+            transaction_data = result.fetchone()
+            if transaction_data is None:
+                raise NotFoundException("Transaction not found")
+            return Transaction(**transaction_data)
